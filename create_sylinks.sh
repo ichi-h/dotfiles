a2f6ed07a44be9148569b884ec1387f98b50ca29
ippee <ippee.music@gmail.com>
2021-09-07 22:45:37 +0900

    Fix url of original directory

--- create_sylinks.sh
+++ create_sylinks.sh
@@ -5,5 +5,5 @@ ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/ignore ~/.config/git/ignore
-ln -s ~/dotfiles/.mycmd/ ~/.mycmd
+ln -s ~/dotfiles/.mycmd ~/.mycmd
 
