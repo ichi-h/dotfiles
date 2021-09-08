5023d6ab03395ab499a9ae053d5e86afe8505f65
ippee <ippee.music@gmail.com>
2021-09-08 21:02:50 +0900

    Add process to change permission

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Create the sylinks of the dotfiles
 links=(~/.gitconfig ~/.zshenv ~/.zshrc ~/.vimrc ~/.config/git/ignore ~/.mycmd)
-
 for link in ${links[@]}; do
   if [ ! -e $link ]; then
     file_name=`basename $link`
     ln -s ~/dotfiles/$file_name $link
     echo Create $link sylink.
   fi
+done
+
+# Change the permission of the script files in .mycmd/bin/
+scripts=`find .mycmd/bin/ -type f`
+for script in ${scripts[@]}; do
+  chmod 700 $script
 done
