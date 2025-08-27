56e1b8b5f7810c0fd9172f26ba17c12061aba926
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-08-27 14:25:36 +0900

    Add nix-ld.

--- modules/base/core.nix
+++ modules/base/core.nix
@@ -59,6 +59,9 @@
   #   enableSSHSupport = true;
   # };
 
+  # Run unpatched dynamic binaries on NixOS.
+  programs.nix-ld.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
