798db17b1d53b999beda5b6a4e114a6d0745065d
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-09-12 13:42:08 +0900

    Add k8s valume directories to Hanaakari.

--- nix/hosts/hanaakari/configuration.nix
+++ nix/hosts/hanaakari/configuration.nix
@@ -25,4 +25,15 @@
   };
 
   users.users."${vars.username}".hashedPassword = impurelibs.secrets.hashed-user-passwd-hanaakari;
+
+  systemd.tmpfiles.rules = [
+    "d /mnt/k8s/volumes/loki 0755 root root -"
+    "d /mnt/k8s/volumes/prometheus/server 0755 root root -"
+    "d /mnt/k8s/volumes/prometheus/alertmanager 0755 root root -"
+    "d /mnt/k8s/volumes/vaultwarden 0755 root root -"
+    "d /mnt/k8s/volumes/shared/data 0770 root root -"
+    "d /mnt/k8s/volumes/shared/config 0755 root root -"
+    "d /mnt/k8s/volumes/shared/custom_apps 0755 root root -"
+    "d /mnt/k8s/volumes/shared/themes 0755 root root -"
+  ];
 }
