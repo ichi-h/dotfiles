3c3afcec93f640556c3cd6816a54234189946dd4
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-09-16 15:19:56 +0900

    Change Gnome to Niri.

--- nix/hosts/hanaakari/configuration.nix
+++ nix/hosts/hanaakari/configuration.nix
@@ -4,25 +4,10 @@
     ./hardware-configuration.nix
     ../../modules/base
     ../../modules/k8s/node.nix
+    ../../modules/desktop
   ];
 
   networking.hostName = "hanaakari"; # Define your hostname.
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm = {
-    enable = true;
-    autoSuspend = false;
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   users.users."${vars.username}".hashedPassword = impurelibs.secrets.hashed-user-passwd-hanaakari;
 
