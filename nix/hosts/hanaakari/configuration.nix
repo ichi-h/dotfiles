a2ba6b3ba5f0f7a6cf79258407ba8f6348617675
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-09-18 09:40:42 +0900

    Remove unused directories.

--- nix/hosts/hanaakari/configuration.nix
+++ nix/hosts/hanaakari/configuration.nix
@@ -14,16 +14,4 @@
   networking.hostName = "hanaakari"; # Define your hostname.
 
   users.users."${vars.username}".hashedPassword = impurelibs.secrets.hashed-user-passwd-hanaakari;
-
-  systemd.tmpfiles.rules = [
-    "d /mnt/k8s/volumes/loki 0755 root root -"
-    "d /mnt/k8s/volumes/prometheus/server 0755 root root -"
-    "d /mnt/k8s/volumes/prometheus/alertmanager 0755 root root -"
-    "d /mnt/k8s/volumes/traefik/data 0755 root root -"
-    "d /mnt/k8s/volumes/vaultwarden 0755 root root -"
-    "d /mnt/k8s/volumes/shared/data 0770 root root -"
-    "d /mnt/k8s/volumes/shared/config 0755 root root -"
-    "d /mnt/k8s/volumes/shared/custom_apps 0755 root root -"
-    "d /mnt/k8s/volumes/shared/themes 0755 root root -"
-  ];
 }
