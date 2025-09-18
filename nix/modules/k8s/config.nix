8b2d1310ea29b29f480f00a30e9aceb8c6c4c049
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-09-18 19:45:21 +0900

    Set Yomogi private address to cluster ip.

--- nix/modules/k8s/config.nix
+++ nix/modules/k8s/config.nix
@@ -2,7 +2,7 @@
 {
   # When using easyCerts=true the IP Address must resolve to the master on creation.
   # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
-  kubeMasterIP = impurelibs.secrets.ip-address-yomogi;
+  kubeMasterIP = impurelibs.secrets.ip-address-yomogi.private;
   kubeMasterHostname = "api.kube";
   kubeMasterAPIServerPort = 6443;
 }
