be5622dd529bc1e0d8f554ebd631b8c031ec83e6
ichi-h <85932406+ichi-h@users.noreply.github.com>
2026-03-12 18:28:09 +0900

    feat(copilot): update github-copilot-cli to version 1.0.4

--- nix/home/base/cli/copilot-cli/default.nix
+++ nix/home/base/cli/copilot-cli/default.nix
@@ -2,14 +2,14 @@
 let
   copilotCli = pkgs.stdenv.mkDerivation {
     pname = "github-copilot-cli";
-    version = "1.0.2";
+    version = "1.0.4";
     src = pkgs.fetchzip {
       url = if pkgs.stdenv.hostPlatform.isDarwin
-        then "https://github.com/github/copilot-cli/releases/download/v1.0.2/copilot-darwin-arm64.tar.gz"
-        else "https://github.com/github/copilot-cli/releases/download/v1.0.2/copilot-linux-x64.tar.gz";
+        then "https://github.com/github/copilot-cli/releases/download/v1.0.4/copilot-darwin-arm64.tar.gz"
+        else "https://github.com/github/copilot-cli/releases/download/v1.0.4/copilot-linux-x64.tar.gz";
       sha256 = if pkgs.stdenv.hostPlatform.isDarwin
-        then "sha256-cIkm5UppYdqOLpXhb0hbOtBRJjXddBpt2LOk8mjyIhY="
-        else "sha256-l1bHnqMe0PdisHM/Z3QyhDf9jrA7PVnTWZKq+duDV3M=";
+        then "sha256-AWeLJBBnasMjWDkG85jNr4X/id+QXajcGGlKl17rvgc="
+        else "sha256-QQhYsVheQqKcNubFKDXnk4q/lfYhV2EWjeHOS1pe9M0=";
     };
     phases = [ "installPhase" ];
     dontBuild = true;
