c66ebf362f8c737cfba0fb232edffc1488b0b762
ichi-h <85932406+ichi-h@users.noreply.github.com>
2026-09-03 16:23:44 +0900

    fix: rename bitwarden to bitwarden-desktop

--- nix/home/linux/gui/desktop-entry.nix
+++ nix/home/linux/gui/desktop-entry.nix
@@ -10,5 +10,5 @@
 
   home.file.".local/share/applications/bitwarden.desktop".text =
     builtins.replaceStrings [ "Exec=bitwarden %U" ] [ ("Exec=bitwarden " + vars.x11-args + " %U") ]
-      (builtins.readFile "${pkgs.bitwarden}/share/applications/bitwarden.desktop");
+      (builtins.readFile "${pkgs.bitwarden-desktop}/share/applications/bitwarden.desktop");
 }
